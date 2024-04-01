2402d1cb650f400def1082b52df9d5cae438f527
KIMMIJU <hviezda1212@gmail.com>
2024-04-01 18:40:05 +0900

    왼쪽 UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Container, Row, Col } from 'react-bootstrap';
+import ContactForm from "./Component/ContactForm";
 
 function App() {
   return (
     <div className="App">
-<h1 className="title">연락처</h1>
+      <h1 className="title">연락처</h1>
+      <Container>
+        <Row>
+          <Col><ContactForm/></Col>
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
